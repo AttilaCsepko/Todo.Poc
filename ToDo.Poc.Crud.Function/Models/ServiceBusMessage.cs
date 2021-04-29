08cecaed17e0aa954fee59f2e455a498fad147d4
AttilaCsepko <attila.csepko@gmail.com>
2021-04-29 16:22:30 +0200

    add models

---
+++ ToDo.Poc.Crud.Function/Models/ServiceBusMessage.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace ToDo.Poc.Crud.Function.Models
+{
+    public class ServiceBusMessage
+    {
+        public string Value { get; set; }
+        public List<object> Formatters { get; set; }
+        public List<object> ContentTypes { get; set; }
+        public int StatusCode { get; set; }
+    }
+}
---
+++ ToDo.Poc.Crud.Function/Models/Todo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ToDo.Poc.Crud.Function.Models
+{
+    public class TodoCreateModel
+    {
+        public string TaskDescription { get; set; }
+    }
+
+    public class TodoUpdateModel
+    {
+        public string TaskDescription { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+
+    public class Todo
+    {
+        public string Id { get; set; } = Nanoid.Nanoid.Generate(size: 10);
+        public DateTime CreatedTime { get; set; } = DateTime.UtcNow;
+        public string TaskDescription { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+}
